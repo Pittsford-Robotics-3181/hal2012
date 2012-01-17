669e16a8e0ff7b0af00f089a624028e1351c66fd
Avaera8820@gmail.com <Avaera8820@gmail.com@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-17 23:59:34 +0000

    The first beginnings of the brand-spankin'-new Dash Management System.

---
+++ nbProject/src/org/frc3181/dash/Dash.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc3181.dash;
+
+/**
+ * A hopefully-useful way to manage the Driver Station Dash.
+ * @author Chris Cheng (2012)
+ */
+public class Dash {
+    
+}

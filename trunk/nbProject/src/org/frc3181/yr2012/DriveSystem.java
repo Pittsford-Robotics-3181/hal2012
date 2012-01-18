870816275e15c94b83bd6868a5ce2120edccd8ea
mrmelville <mrmelville@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-18 00:45:51 +0000

    Deleted last year's code in HAL, added DriveSystem and Hardware.

---
+++ trunk/nbProject/src/org/frc3181/yr2012/DriveSystem.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc3181.yr2012;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ * This is our robot's drive system, which implements mecanum wheels.
+ * 
+ * Drive System:
+ * Boolean trigger on Joystick:
+ * On=Rotate Robot (to align for a crash)
+ * Off=Linear movement of  Robot
+ * Rotational mode inverts two motors, using RobotDrive.setInvertedMotor(the motor, Joystick.getTrigger());
+ * Use RobotDrive.mecanumDrive_Polar(Joystick.getMagnitude(), Joystick.getDirectionDegrees(), Speed);
+ * May use another Joystick for the speed, or just a constant.
+ * @author lowenstein benjamin
+ * @author robbiemarkwick
+ */
+public class DriveSystem extends RobotDrive{
+    
+    /**
+     * Constructor
+     * @param frontLeftMotor
+     * @param rearLeftMotor
+     * @param frontRightMotor
+     * @param rearRightMotor 
+     */
+    public DriveSystem(SpeedController frontLeftMotor, SpeedController rearLeftMotor, SpeedController frontRightMotor, SpeedController rearRightMotor){
+        super(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+    }
+    
+    /**
+     * Given a speed between -1.0 and 1.0, drive forward (if positive) or backward (if positive).
+     * @param speed The speed to drive at.
+     */
+    public void driveForwardBackward(double speed){
+        if(speed > 0)
+            mecanumDrive(speed, 0);
+        else
+            mecanumDrive(speed, 180);
+    }
+    
+    /**
+     * Given a speed between -1.0 and 1.0, drive right (if positive) or left (if positive).
+     * @param speed The speed to drive at.
+     */
+    public void driveLeftRight(double speed){
+        if(speed > 0)
+            mecanumDrive(speed, 270);
+        else
+            mecanumDrive(speed, 90);
+    }
+    
+    /**
+     * Drive method for Mecanum wheeled robots. A method for driving with
+     * Mecanum wheeled robots. There are 4 wheels on the robot, arranged so that
+     * the front and back wheels are toed in 45 degrees. When looking at the
+     * wheels from the top, the roller axles should form an X across the robot. 
+     * @param magnitude The speed that the robot should drive in a given direction.
+     * @param direction The direction the robot should drive in degrees. The direction and magnitude are independent of the rotation rate.
+     */
+    private void mecanumDrive(double magnitude, double direction){
+        mecanumDrive_Polar(magnitude, direction, 0);
+    }
+}
--- trunk/nbProject/src/org/frc3181/yr2012/HAL.java
+++ trunk/nbProject/src/org/frc3181/yr2012/HAL.java
@@ -1,18 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.frc3181.yr2012;
 
-
-import edu.wpi.first.wpilibj.AnalogModule;
-import edu.wpi.first.wpilibj.Dashboard;
-import edu.wpi.first.wpilibj.DigitalModule;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStationEnhancedIO.EnhancedIOException;
-import edu.wpi.first.wpilibj.DriverStationEnhancedIO.tAccelChannel;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Solenoid;
 
 /**
  * The main class for our robot
@@ -23,76 +11,37 @@ import edu.wpi.first.wpilibj.Solenoid;
  * @author Robbie
  */
 
-
-
-/**
- * Drive System:
- * Boolean trigger on Joystick:
- * On=Rotate Robot (to align for a crash)
- * Off=Linear movement of  Robot
- * Rotational mode inverts two motors, using RobotDrive.setInvertedMotor(the motor, Joystick.getTrigger());
- * Use RobotDrive.mecanumDrive_Polar(Joystick.getMagnitude(), Joystick.getDirectionDegrees(), Speed);
- * May use another Joystick for the speed, or just a constant.
- * @author robbiemarkwick
- */
 public class HAL extends IterativeRobot {
-    /*
-     * // Autonomous mode
-   
-    int autonoMode = 0;
-
-    public static double ana2; // Buttons 1-6
-    public static double ana3; // Buttons 7-10
-    public static double ana1; // Buttons 12-14
-    public static boolean digital2; // Arm control
-    public static boolean digital4; // Three-way toggle for lifter
-    public static boolean digital6; // Three-way toggle for lifter
-    public static boolean digital8; // Minibot deployment
-    // Accelerometer values
-    public static double AccelX;
-    public static double AccelY;
-    public static double AccelZ;
-*/
 
     /**
      * This function is run when the robot starts up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        
        
     }
 
     /**
-     * This function is called periodically during disabled. It determines the
-     * autonomous mode that we want, based on the Cypress accelerometer.
+     * This function is called periodically during disabled.
      */
     public void disabledPeriodic() {
-        updateEnhancedIO();
-
-        // Determine which autonomous mode we want and tell the drivers
+        
     }
 
 
     //------------$*$*$*$*$*$*$*$*AUTONOMOUS METHODS*$*$*$*$*$*$*$*$------------//
 
     /**
-     * This function is run when autonomous mode begins. It determines which
-     * autonomous mode we are using and starts the timer and compressor. It also
-     * updating the EnhancedIO inputs.
+     * This function is run when autonomous mode begins.
      */
     public void autonomousInit() {
-       
         
     }
 
     /**
-     * This function is called periodically during autonomous. It runs the
-     * selected autonomous mode.
+     * This function is called periodically during autonomous.
      */
     public void autonomousPeriodic() {
-
-        updateDashboard();
         
     }
 
@@ -100,13 +49,10 @@ public class HAL extends IterativeRobot {
     //------------$*$*$*$*$*$*$*$*TELEOP METHODS*$*$*$*$*$*$*$*$------------//
 
     /**
-     * This function is run when teleop mode begins. It starts the timer and
-     * compressor and updates the EnhancedIO inputs.
+     * This function is run when teleop mode begins.
      */
     public void teleopInit() {
         
-
-        updateEnhancedIO();
     }
 
     /**
@@ -114,143 +60,5 @@ public class HAL extends IterativeRobot {
      */
     public void teleopPeriodic() {
         
-        updateDashboard();
-        updateEnhancedIO();
-
-        //**************** BEGIN DRIVE CODE ****************//
-        
-        //**************** END DRIVE CODE ****************//
-
-
-
-        //System.out.print("Button Up:" + getBoxButton(17) + " Button Down:" + getBoxButton(16) + "\n");
-
-        // Check if the minibot is "unlocked"
-        
-        // Control the arm, claw, and lifter
-        
-        // Check if Minibot is to be deployed or retracted.
-        
-    }
-    
-
-     // <editor-fold defaultstate="collapsed" desc="void HAL.updateDashboard()">
-    /**
-     * Shows data on driver station dashboard. For an explanation, see link:
-     * http://www.chiefdelphi.com/forums/showthread.php?t=90167
-     */
-    void updateDashboard() {
-        Dashboard lowDashData = DriverStation.getInstance().getDashboardPackerLow();
-        lowDashData.addCluster();
-        {
-            lowDashData.addCluster();
-            {     //analog modules
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 8; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(1).getAverageVoltage(i));
-                    }
-                }
-                lowDashData.finalizeCluster();
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 8; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(2).getAverageVoltage(i));
-                    }
-                }
-                lowDashData.finalizeCluster();
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addCluster();
-            { //digital modules
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 4;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 6;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayReverse());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addByte(Solenoid.getAllFromDefaultModule());
-
-        }
-        lowDashData.finalizeCluster();
-        lowDashData.commit();
-
-    }
-    // </editor-fold>
-
-
-    /**
-     * Update the analog and digital inputs from the EnhancedIO module.
-     */
-    public void updateEnhancedIO() {
-       /*
-        try{
-            ana1 = Hardware.dseio.getAnalogIn(1);
-            ana3 = Hardware.dseio.getAnalogIn(3);
-            ana2 = Hardware.dseio.getAnalogIn(2);
-            digital2 = Hardware.dseio.getDigital(2);
-            digital4 = Hardware.dseio.getDigital(4);
-            digital6 = Hardware.dseio.getDigital(6);
-            digital8 = Hardware.dseio.getDigital(8);
-            AccelX = Hardware.dseio.getAcceleration(tAccelChannel.kAccelX);
-            AccelY = Hardware.dseio.getAcceleration(tAccelChannel.kAccelY);
-            AccelZ = Hardware.dseio.getAcceleration(tAccelChannel.kAccelZ);
-        } catch (EnhancedIOException ex) {
-            System.out.println(ex);
-        }
-             * */
-             
-    }
-
-    /**
-     * This checks whether a specific button on the button box is being pressed
-     * (this can also check the toggle switches). It is unclear why the bounds
-     * for ana2 overlap.
-     * @param button Which button (or toggle) to check
-     * @return Whether the button is being pressed, or the toggle is true.
-     */
-    public static boolean getBoxButton(int button) {
-
-        return false;
     }
 }

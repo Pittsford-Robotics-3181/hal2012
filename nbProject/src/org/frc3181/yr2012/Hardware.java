41862dec669b63ddfb7cea3bc468dd41f3306de9
mrmelville <mrmelville@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-18 00:45:51 +0000

    Deleted last year's code in HAL, added DriveSystem and Hardware.

---
+++ nbProject/src/org/frc3181/yr2012/Hardware.java
@@ -0,0 +1,21 @@
+package org.frc3181.yr2012;
+
+import edu.wpi.first.wpilibj.Jaguar;
+
+/**
+ * This file contains our hardware declarations.
+ * @author lowenstein benjamin
+ */
+public class Hardware {
+    // motors
+    public static Jaguar frontLeftMotor = new Jaguar(/*slot*/2, /*channel*/1);
+    public static Jaguar rearLeftMotor = new Jaguar(/*slot*/2, /*channel*/2);
+    public static Jaguar frontRightMotor = new Jaguar(/*slot*/2, /*channel*/3);
+    public static Jaguar rearRightMotor = new Jaguar(/*slot*/2, /*channel*/4);
+    
+    // drive system
+    public static DriveSystem driveSystem = new DriveSystem(Hardware.frontLeftMotor,
+                                                            Hardware.rearLeftMotor,
+                                                            Hardware.frontRightMotor,
+                                                            Hardware.rearRightMotor);
+}

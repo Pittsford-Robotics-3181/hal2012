c0fda20f5972457cc9e69b56d6ede65bd9369844
Avaera8820@gmail.com <Avaera8820@gmail.com@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-20 01:59:45 +0000

    Jags are on slot *4*, Ben.

--- trunk/nbProject/src/org/frc3181/yr2012/Hardware.java
+++ trunk/nbProject/src/org/frc3181/yr2012/Hardware.java
@@ -9,13 +9,13 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class Hardware {
     // motors
-    public static Jaguar frontLeftMotor = new Jaguar(/*slot*/2, /*channel*/1);
-    public static Jaguar rearLeftMotor = new Jaguar(/*slot*/2, /*channel*/2);
-    public static Jaguar frontRightMotor = new Jaguar(/*slot*/2, /*channel*/3);
-    public static Jaguar rearRightMotor = new Jaguar(/*slot*/2, /*channel*/4);
+    public static Jaguar frontLeftMotor = new Jaguar(/*slot*/4, /*channel*/1);
+    public static Jaguar rearLeftMotor = new Jaguar(/*slot*/4, /*channel*/2);
+    public static Jaguar frontRightMotor = new Jaguar(/*slot*/4, /*channel*/3);
+    public static Jaguar rearRightMotor = new Jaguar(/*slot*/4, /*channel*/4);
     
     //joystick
-    public static Joystick driveController = new Joystick(01);
+    public static Joystick driveController = new Joystick(1);
     
     // drive system
     public static DriveSystem driveSystem = new DriveSystem(Hardware.frontLeftMotor,

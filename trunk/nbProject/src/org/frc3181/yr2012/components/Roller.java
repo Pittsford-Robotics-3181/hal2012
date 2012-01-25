d43cbda6bf8f0c8d0c5756b9dd77b127fddc4abf
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-25 13:05:18 +0000

    Added comments for Roller and Shooter. Also added a line of code to stop the aiming during a shot.

--- trunk/nbProject/src/org/frc3181/yr2012/components/Roller.java
+++ trunk/nbProject/src/org/frc3181/yr2012/components/Roller.java
@@ -10,14 +10,26 @@ import edu.wpi.first.wpilibj.Timer;
  * @author Ben
  */
 public class Roller {
+    
+    /** 
+     * Two Fields
+     * Collector: controls the motor that collects the balls
+     * debouncer: gives the driver time to pull finger off of button.
+     */
     private Victor Collector;
     private Timer debouncer=new Timer();
     
+     /** 
+     * Constructs the Roller using the given motor.
+     */
     public Roller(Victor Collect){
         Collector=Collect;
     }
     
-    public void RollerController(){
+    /**
+     * rollerController allows the driver to turn the Roller on and off
+     */
+    public void rollerController(){
        if(Hardware.shotController.getRawButton(2)&&debouncer.get()>=500000){
            debouncer.reset();
            debouncer.start();

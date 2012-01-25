c1fbe167d9ebfed367f15ee0a19535923ba85dfb
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-25 00:53:12 +0000

    Wrote code for shooter (only shoots if a ball is in the shooter, though).

--- trunk/nbProject/src/org/frc3181/yr2012/components/Shooter.java
+++ trunk/nbProject/src/org/frc3181/yr2012/components/Shooter.java
@@ -1,9 +1,40 @@
 package org.frc3181.yr2012.components;
-
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Victor;
+import org.frc3181.yr2012.Hardware;
+import edu.wpi.first.wpilibj.Timer;
 /**
  * The shooter.
+ * @author Robbie
  * @author Ben
  */
 public class Shooter {
+    /**
+     * Three Variables:
+     * aim: the Victor that controls the angle
+     * cannon: the Victor that controls the wheels to launch the balls
+     * delay: the timer that delays any shooter action during a shot.
+     */
+    private Victor aim=Hardware.aimShooter;
+    private Victor cannon=Hardware.launchBalls;
+    private Timer delay=new Timer();
     
+    private void aimShot(double anglechange) {
+    aim.set(anglechange);
+    }
+    
+    private void Shoot() {
+    cannon.set(1);
+    delay.reset();
+    delay.start();
+    }
+    
+    public void controlShooting(){
+        if (delay.get()>=1000000){
+            cannon.set(0);
+            aimShot(-Hardware.shotController.getY());
+            if(Hardware.shotController.getTrigger())
+                Shoot();
+        }
+    }
 }

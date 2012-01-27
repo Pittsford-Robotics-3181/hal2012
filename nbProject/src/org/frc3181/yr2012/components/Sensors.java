4b6ccfaad9ef42910df5336baf6f88633ad76c9f
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-27 01:46:48 +0000

    Listed sensors needed

--- nbProject/src/org/frc3181/yr2012/components/Sensors.java
+++ nbProject/src/org/frc3181/yr2012/components/Sensors.java
@@ -4,6 +4,15 @@ package org.frc3181.yr2012.components;
  * Our sensors.
  * @author Ben
  */
+
+/**
+ * Sensors needed for Tipper:
+ * Detect if full up
+ * Detect if full down
+ * Detect if front of tipper is over bridge (slow down and prepare to tip)
+ * Detect if back of tipper is over to bridge (ready to tip)
+ * @author robbiemarkwick
+ */
 public class Sensors {
     
 }

277cebc4fe182a806eb56b22d2072f135c4f6c46
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-27 01:02:51 +0000

    Added Wheel and Fixed Roller

--- trunk/nbProject/src/org/frc3181/yr2012/components/Wheel.java
+++ trunk/nbProject/src/org/frc3181/yr2012/components/Wheel.java
@@ -1,9 +1,19 @@
 package org.frc3181.yr2012.components;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * The wheel that moves balls up the robot.
+ * @author Robbie
  * @author Ben
  */
 public class Wheel {
+    private Victor mover;
     
+    public Wheel(Victor spin){
+        mover=spin;
+    }
+    
+    public void moveBalls (){
+        mover.set(1.00);
+    }
 }

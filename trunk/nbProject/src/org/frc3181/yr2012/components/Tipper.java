2a548552144e8d9c82d4ee74b3ad78d5bec572dc
mrmelville <mrmelville@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-28 18:16:33 +0000

    Stylized Tipper.

--- trunk/nbProject/src/org/frc3181/yr2012/components/Tipper.java
+++ trunk/nbProject/src/org/frc3181/yr2012/components/Tipper.java
@@ -1,131 +1,146 @@
 package org.frc3181.yr2012.components;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
+
 import edu.wpi.first.wpilibj.Victor;
 import org.frc3181.yr2012.Hardware;
+
 /**
  * The mechanism that tips the bridge.
  * @author Robbie
  * @author Ben
  */
 public class Tipper {
-    /*
-     * Four fields:
-     * Tip: the Victor controlling the Tipper
-     * Pull: the Victor that pulls the tipper in.
-     * clock: controls time
-     * running: is the tipper operating?
-     * down: is the tipper down or moving down?
-     */
-    private Victor Tip;
-    private Victor Pull;
-    private int targetDistance=1;
-    private boolean up;
-    private boolean retracting=false;
-    private boolean extending=false;
-    private boolean running=false;
+
+    //Height constants
+    //These WILL be changed.
+    public static final double TIPPER_UP = 1;
+    public static final double TIPPER_HALF_UP = 2;
+    public static final double TIPPER_HALF_DOWN = 3;
+    public static final double TIPPER_DOWN = 4;    
     
-    /*
-     * Constructs a Tipper
-     * @param TipMotor The victor used for Tip.
+    private Victor tipperLifter;
+    private Victor retractor;
+    private double targetHeight = TIPPER_UP;
+    private boolean up = true;
+    private boolean retracting = false;
+    private boolean extending = false;
+    private boolean running = false;
+
+    /**
+     * Tipper constructor.
+     * @param tipMotor The motor that raises and lowers the tipper.
+     * @param pullMotor The motor that retracts and extends the tipper.
      */
-    public Tipper(Victor TipMotor, Victor PullMotor){
-    Tip=TipMotor;
-    Pull=PullMotor;
+    public Tipper(Victor tipMotor, Victor pullMotor) {
+        tipperLifter = tipMotor;
+        retractor = pullMotor;
     }
-    
-    /*
-     * controls the tipper.
-     * if the 3 button is pressed, the tipper changes position.
+
+    /**
+     * Actually tips the bridge, after ensuring the robot is not driving.
      */
-    private void TipBridge(){
+    private void TipBridge() {
         Hardware.driveSystem.setStop(true);
-        running=true;
-        Tip.set(-1);
-        targetDistance=4; 
-        up=false;
+        running = true;
+        tipperLifter.set(-1);
+        targetHeight = TIPPER_DOWN;
+        up = false;
     }
-    private void readTipControls(){
-        if(Hardware.driveController.getRawButton(3))
-        {
-            running=true;
-            Tip.set((1<Hardware.sensorSet.findFoot()) ?1:-1);
-            targetDistance=1; 
-            up=(1<Hardware.sensorSet.findFoot());
+
+    /**
+     * Figure out what buttons are being pushed on joystick and act appropriately.
+     */
+    private void readTipControls() {
+        if (Hardware.driveController.getRawButton(3)) {
+            running = true;
+            tipperLifter.set((TIPPER_UP < Hardware.sensorSet.findFoot()) ? 1 : -1);
+            targetHeight = TIPPER_UP;
+            up = (TIPPER_UP < Hardware.sensorSet.findFoot());
+        } else if (Hardware.driveController.getRawButton(8)) {
+            running = true;
+            tipperLifter.set((TIPPER_HALF_UP < Hardware.sensorSet.findFoot()) ? 1 : -1);
+            targetHeight = TIPPER_HALF_UP;
+            up = (TIPPER_HALF_UP < Hardware.sensorSet.findFoot());
+        } else if (Hardware.driveController.getRawButton(9)) {
+            running = true;
+            tipperLifter.set((TIPPER_HALF_DOWN < Hardware.sensorSet.findFoot()) ? 1 : -1);
+            targetHeight = TIPPER_HALF_DOWN;
+            up = (TIPPER_HALF_DOWN < Hardware.sensorSet.findFoot());
+        } else if (Hardware.driveController.getRawButton(2)) {
+            running = true;
+            tipperLifter.set((TIPPER_DOWN < Hardware.sensorSet.findFoot()) ? 1 : -1);
+            targetHeight = TIPPER_DOWN;
+            up = (TIPPER_DOWN < Hardware.sensorSet.findFoot());
         }
-        else if(Hardware.driveController.getRawButton(8))
-        {
-            running=true;
-            Tip.set((2<Hardware.sensorSet.findFoot()) ?1:-1);
-            targetDistance=2; 
-            up=(2<Hardware.sensorSet.findFoot());
-        }
-         else if(Hardware.driveController.getRawButton(9))
-        {
-            running=true;
-            Tip.set((3<Hardware.sensorSet.findFoot()) ?1:-1);
-            targetDistance=3; 
-            up=(3<Hardware.sensorSet.findFoot());
-        }
-         else if(Hardware.driveController.getRawButton(2))
-        {
-            running=true;
-            Tip.set((4<Hardware.sensorSet.findFoot()) ?1:-1);
-            targetDistance=4; 
-            up=(4<Hardware.sensorSet.findFoot());
-        }
-        if(Hardware.driveController.getRawButton(7))
-            retractTipper();
-        else if(Hardware.driveController.getRawButton(7))
+        if (Hardware.driveController.getRawButton(7)) {
+            moveTipperUp();
+        } else if (Hardware.driveController.getRawButton(7)) {
             extend();
-    }
-    private void retractTipper(){
-         retracting=true;
-        running=true;
-            Tip.set(1);
-            targetDistance=1; 
-            up=(true);
-    }
-    private void retract(){
-        if(!Hardware.sensorSet.in.get()){
-        Pull.set(1);
-        retracting=true;
-        }
-        else{
-            Pull.set(0);
-            retracting=false;
         }
     }
-    private void extend(){
-        if(!Hardware.sensorSet.out.get()){
-        Pull.set(-1);
-        }
-        else{
-            Pull.set(0);
-            extending=false;
+
+    /**
+     * Move the tipper up.
+     */
+    private void moveTipperUp() {
+        retracting = true;
+        running = true;
+        tipperLifter.set(1);
+        targetHeight = TIPPER_UP;
+        up = true;
+    }
+    
+    /**
+     * Retract tipper.
+     */
+    private void retract() {
+        if (!Hardware.sensorSet.in.get()) {
+            retractor.set(1);
+            retracting = true;
+        } else {
+            retractor.set(0);
+            retracting = false;
         }
     }
-    public void controlTipper(){
-        if(running){
-            if(((targetDistance<=Hardware.sensorSet.findFoot())&&(up))||((targetDistance>=Hardware.sensorSet.findFoot())&&(!up))){
-                    running=false;
-                    Tip.set(0);
-                }
+
+    /**
+     * Extend tipper.
+     */
+    private void extend() {
+        if (!Hardware.sensorSet.out.get()) {
+            retractor.set(-1);
+        } else {
+            retractor.set(0);
+            extending = false;
         }
-        else if(retracting){
+    }
+
+    /**
+     * Control tipper.
+     */
+    public void controlTipper() {
+        if (running) {
+            if (((targetHeight <= Hardware.sensorSet.findFoot()) && (up)) || ((targetHeight >= Hardware.sensorSet.findFoot()) && (!up))) {
+                running = false;
+                tipperLifter.set(0);
+            }
+        } else if (retracting) {
             retract();
-        }
-        else if(extending){
-        extend();
-        }
-        else{
-            switch(Hardware.sensorSet.findBridge()){
-                case 0: Hardware.driveSystem.setSlow(false); readTipControls(); break;
-                case 1: Hardware.driveSystem.setSlow(true); break;
+        } else if (extending) {
+            extend();
+        } else {
+            switch (Hardware.sensorSet.findBridge()) {
+                case 0:
+                    Hardware.driveSystem.setSlow(false);
+                    readTipControls();
+                    break;
+                case 1:
+                    Hardware.driveSystem.setSlow(true);
+                    break;
                 case 2: {
-                            Hardware.driveSystem.setStop(true); 
-                            TipBridge(); 
-                            break;
-                        }
+                    Hardware.driveSystem.setStop(true);
+                    TipBridge();
+                    break;
+                }
             }
         }
     }

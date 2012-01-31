7d5a6a814db9b7116265316e09dc0b677bf398cf
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-01-31 00:48:41 +0000

    Fixed Tipper

--- nbProject/src/org/frc3181/yr2012/components/Tipper.java
+++ nbProject/src/org/frc3181/yr2012/components/Tipper.java
@@ -14,13 +14,10 @@ public class Tipper {
     //These WILL be changed.
     public static final double TIPPER_BACK= 0;
     public static final double TIPPER_UP = 1;
-    public static final double TIPPER_HALF_UP = 2;
-    public static final double TIPPER_HALF_DOWN = 3;
-    public static final double TIPPER_DOWN = 4;
+    public static final double TIPPER_HALF = 2;
+    public static final double TIPPER_DOWN = 3;
     private Victor tipperLifter;
     private double targetHeight = TIPPER_BACK;
-    private boolean goDown = true;
-    private boolean running = false;
 
     /**
      * Tipper constructor.
@@ -36,10 +33,8 @@ public class Tipper {
      */
     private void tipBridge() {
         Hardware.driveSystem.setStop(true);
-        running = true;
         tipperLifter.set(-1);
         targetHeight = TIPPER_DOWN;
-        goDown = true;
     }
 
     /**
@@ -47,31 +42,18 @@ public class Tipper {
      */
     private void readTipControls() {
         if (Hardware.driveJoystick.getRawButton(3)) {
-            running = true;
             tipperLifter.set((TIPPER_UP < Sensors.getTipperDistance()) ? 1 : -1);
             targetHeight = TIPPER_UP;
-            goDown = (TIPPER_UP < Sensors.getTipperDistance());
-        } else if (Hardware.driveJoystick.getRawButton(8)) {
-            running = true;
-            tipperLifter.set((TIPPER_HALF_UP < Sensors.getTipperDistance()) ? 1 : -1);
-            targetHeight = TIPPER_HALF_UP;
-            goDown = (TIPPER_HALF_UP < Sensors.getTipperDistance());
-        } else if (Hardware.driveJoystick.getRawButton(9)) {
-            running = true;
-            tipperLifter.set((TIPPER_HALF_DOWN < Sensors.getTipperDistance()) ? 1 : -1);
-            targetHeight = TIPPER_HALF_DOWN;
-            goDown = (TIPPER_HALF_DOWN < Sensors.getTipperDistance());
+        } else if (Hardware.driveJoystick.getRawButton(6)) {
+            tipperLifter.set((TIPPER_HALF < Sensors.getTipperDistance()) ? 1 : -1);
+            targetHeight = TIPPER_HALF;
         } else if (Hardware.driveJoystick.getRawButton(2)) {
-            running = true;
             tipperLifter.set((TIPPER_DOWN < Sensors.getTipperDistance()) ? 1 : -1);
             targetHeight = TIPPER_DOWN;
-            goDown = (TIPPER_DOWN < Sensors.getTipperDistance());
         }
          else if (Hardware.driveJoystick.getRawButton(7)) {
-            running = true;
             tipperLifter.set((TIPPER_BACK < Sensors.getTipperDistance()) ? 1 : -1);
             targetHeight = TIPPER_BACK;
-            goDown= (TIPPER_BACK < Sensors.getTipperDistance());
         }
     }
 
@@ -79,14 +61,8 @@ public class Tipper {
      * Control tipper.
      */
     public void controlTipper() {
-        if (running) {
-            if (((targetHeight <= Sensors.getTipperDistance()) && (goDown)) || ((targetHeight >= Sensors.getTipperDistance()) && (!goDown))) {
-                running = false;
-                tipperLifter.set(0);
-            }
-
-        }else {
-            if(Sensors.getTipperDistance()==TIPPER_HALF_UP){
+            tipperLifter.set(.25*(Sensors.getTipperDistance()-targetHeight));
+            if(Sensors.getTipperDistance()==TIPPER_HALF){
             switch (Sensors.senseBridge()) {
                 case 0:
                     Hardware.driveSystem.setSlow(false);
@@ -104,4 +80,3 @@ public class Tipper {
             }
         }
     }
-}

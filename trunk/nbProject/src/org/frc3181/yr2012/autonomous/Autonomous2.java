18aad4565f5e923c201e82bf8ff71eb06ef991f3
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-02-01 01:35:21 +0000

    Changed Variable Names.

--- trunk/nbProject/src/org/frc3181/yr2012/autonomous/Autonomous2.java
+++ trunk/nbProject/src/org/frc3181/yr2012/autonomous/Autonomous2.java
@@ -15,18 +15,18 @@ public class Autonomous2 {
      * TODO: NAME THE VARIABLES ACCORDINGLY BECAUSE I CAN'T UNDERSTAND THEM
      * AND DO IT FAST 
      */
-    private static boolean b=false;
-    private static boolean c=false;
+    private static boolean foundBridge=false;
+    private static boolean tippedBridge=false;
     public static void run(){
-        if(b){
+        if(foundBridge){
         Hardware.bridgeTip.autoHorizontal();
         Hardware.driveSystem.autodrive(2,0,0);
-        b=Hardware.bridgeTip.autoTip(false);
+        foundBridge=Hardware.bridgeTip.autoTip(false);
         }
-        if(b&&!c){
-            c=Hardware.bridgeTip.autoTip(true);
+        if(foundBridge&&!tippedBridge){
+            tippedBridge=Hardware.bridgeTip.autoTip(true);
         }
-        if (c){
+        if (tippedBridge){
             Hardware.driveSystem.autodrive(0.001,180,0);
         }
     }

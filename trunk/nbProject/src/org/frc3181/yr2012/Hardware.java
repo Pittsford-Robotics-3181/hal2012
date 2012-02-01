4be4c982014e54ddcecfec1fdac6bcd2b28ff945
mrmelville <mrmelville@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-02-01 01:27:14 +0000

    Drastically modified shooting. This code will likely not work, and it would be nice to test it, but the robot doesn't exist.

--- trunk/nbProject/src/org/frc3181/yr2012/Hardware.java
+++ trunk/nbProject/src/org/frc3181/yr2012/Hardware.java
@@ -1,10 +1,10 @@
 package org.frc3181.yr2012;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Kinect;
-import edu.wpi.first.wpilibj.KinectStick;
 import org.frc3181.yr2012.components.*;
 
 /**
@@ -31,12 +31,15 @@ public class Hardware {
     public static Jaguar rearRightMotor = new Jaguar(/*slot*/1, /*channel*/ 4);
     // Non-Driving Motors
     public static final Victor aimShooter = new Victor(1, 5);
-    public static final Victor launchBalls = new Victor(1, 6);
+    public static final Victor leftShooter = new Victor(1, 6);
+    public static final Victor rightShooter = new Victor(1, 10);
     public static final Jaguar roller = new Jaguar(1, 7);
     public static final Victor tip = new Victor(1, 8);
     public static final Jaguar conveyorMotor = new Jaguar(1, 9);
     
+    //Kinect!
     public static Kinect kinect = Kinect.getInstance();
+
     // Drive system
     public static DriveSystem driveSystem = new DriveSystem(Hardware.frontLeftMotor,
             Hardware.rearLeftMotor,
@@ -49,13 +52,15 @@ public class Hardware {
     public static Joystick ballJoystick = new Joystick(2);
     
     //Shooter
-    public static Shooter ballLauncher = new Shooter(aimShooter, launchBalls);
+    public static Shooter ballLauncher = new Shooter(aimShooter, leftShooter, rightShooter);
     // Roller
     public static Roller collector = new Roller(roller);
     // Tipper
     public static Tipper bridgeTip = new Tipper(tip);
     // Conveyor
     public static Conveyor ballLift = new Conveyor(conveyorMotor);
+    
+
 
     //sensors are in Sensors.java
 

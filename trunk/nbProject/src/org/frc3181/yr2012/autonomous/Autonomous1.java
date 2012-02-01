81ac56d945339c3562954d4b225c5d0ad39973a3
rmarkwick@frontiernet.net <rmarkwick@frontiernet.net@1260e1ec-5a28-2075-9274-df1720e0c380>
2012-02-01 00:30:19 +0000

    Created Autonomous Package

---
+++ trunk/nbProject/src/org/frc3181/yr2012/autonomous/Autonomous1.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc3181.yr2012.autonomous;
+
+/**
+ *Don't Do anything. Just sit there and have everyone else complain
+ * @author robbiemarkwick
+ */
+public class Autonomous1 {
+    //Do nothing
+}
